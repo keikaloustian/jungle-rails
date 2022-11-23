1863411c36edfecd6178a99ee59be1bdec5b14b4
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-11-22 16:12:43 -0800

    Write and test Product Details spec on Cypress

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,10 @@
+describe("Product Details", () => {
+  it("should visit home page", () => {
+    cy.visit("/");
+  });
+
+  it("should click on a product", () => {
+    cy.get(".products article").first().click();
+    cy.get(".products-show").should("exist");
+  });
+});
